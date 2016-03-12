9476c464376733a1e5209bc9f9f20246593143da
Pes2009 <mmadej@sigma.ug.edu.pl>
2016-03-12 18:25:04 +0100

    prosty program losujacy.

---
+++ losowanie.py
@@ -0,0 +1,13 @@
+import random
+liczby = [1,2,10,24,64,30,24,20]
+
+print random.choice(range(0, 100, 1))
+
+print random.choice(liczby)
+
+tab = ['ada','adam','dominika','andrzej','krzysiek','zofia']
+print tab
+
+tab.sort()
+
+print random.choice(tab)
